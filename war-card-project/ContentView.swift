9f2ac4bf2512588189f4e92b995f7a5b2d418309
Sanchita-36 <sanchitalachke@gmail.com>
2024-11-27 18:14:41 +0530

    UI Construction

--- war-card-project/ContentView.swift
+++ war-card-project/ContentView.swift
@@ -9,13 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Image("card3")
+                    Spacer()
+                }
+                
+                Spacer()
+                Button(action: {
+                    print("I am deal")
+                }, label: {
+                    Image("dealbutton")
+                })
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
