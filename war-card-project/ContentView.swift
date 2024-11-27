3051a4cb8c5ce27873580b7d35345ef9396f229e
Sanchita-36 <sanchitalachke@gmail.com>
2024-11-27 18:02:35 +0530

    Initial Commit

---
+++ war-card-project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  war-card-project
+//
+//  Created by sanchita lachke on 27/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ war-card-project/war_card_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_projectApp.swift
+//  war-card-project
+//
+//  Created by sanchita lachke on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct war_card_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
